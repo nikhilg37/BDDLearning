b3d06690b0b41bd8706c09febc6cdddbe4f94f62
Nikhil <itsnikhilg@gmail.com>
2019-07-31 18:38:22 +0200

    new

--- Learning_BDD/Learning/src/test/java/StepDefs/Login.java
+++ Learning_BDD/Learning/src/test/java/StepDefs/Login.java
@@ -1,9 +1,6 @@
 package StepDefs;
 
-import cucumber.api.*;
-import cucumber.runner.*;
-import cucumber.runtime.*;
-import cucumber.util.*;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
@@ -20,28 +17,28 @@ public class Login {
 	@Given("I navigate to login page")
 	public void navigate_to_loginpage() {
 		// Write code here that turns the phrase above into concrete actions
-
-		throw new cucumber.api.PendingException();
+		System.out.println("login page");
 	}
 
 
 	@When("I enter username")
 	public void i_enter_username() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new cucumber.api.PendingException();
+		// Write code here that turns the phrase abothrow new cucumber.api.PendingException();
+		
 	}
 
 	@When("I enter password")
 	public void i_enter_password() {
+		System.out.println("password");
 		// Write code here that turns the phrase above into concrete actions
-		throw new cucumber.api.PendingException();
+		
 
 	}
 	@Then("I need to be in homepage")
 	public void i_need_to_be_in_homepage() {
+		System.out.println("homepage");
 		// Write code here that turns the phrase above into concrete actions
-		throw new cucumber.api.PendingException();
-
+		
 	}
 	
 }
